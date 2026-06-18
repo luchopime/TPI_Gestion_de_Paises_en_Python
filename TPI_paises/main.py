ed007cfd89ec5f8819b34f35561fadd95e250ab0
luchopime <luchopime1232@gmail.com>
2026-06-18 12:05:18 -0300

    carga de diccionario y menu agregado

---
+++ TPI_paises/main.py
@@ -0,0 +1,21 @@
+from funciones import *
+
+paises = cargar_csv("TPI_paises/paises.csv")
+
+while True:
+    menu()
+
+    opcion = input("Seleccione una opcion: ")
+
+    if opcion == "1":
+        print("Agregar pais")
+
+    elif opcion == "2":
+        print("Actualizar pais")
+
+    elif opcion == "0":
+        print("Fin del programa")
+        break
+
+    else:
+        print("Opcion invalida")
