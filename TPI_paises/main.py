3b9e4a6f6659e3860116b8ba0caf4002866a5f76
dania183-afk <gomezdania183@gmail.com>
2026-06-21 15:55:03 -0300

    Agregué lo que faltaba del menú en el escript main.py

--- TPI_paises/main.py
+++ TPI_paises/main.py
@@ -13,6 +13,21 @@ while True:
     elif opcion == "2":
         print("Actualizar pais")
 
+    elif opcion == "3":
+        print("Buscar pais")
+
+    elif opcion == "4":
+        print("Filtrar paises")
+
+    elif opcion == "5":
+        print("Ordenar paises")    
+
+    elif opcion == "6":
+        print("Estadisticas")
+    
+    elif opcion == "7":
+        print("Mostrar todos")
+
     elif opcion == "0":
         print("Fin del programa")
         break
